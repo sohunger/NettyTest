325462f4e97807ebd5f725aeb96991303b483c6f
sohunger <84076257+sohunger@users.noreply.github.com>
2022-07-21 21:52:37 +0800

    新增请求和响应实体类

--- src/main/java/com/huang/nettyTest/dto/RequestDto.java
+++ src/main/java/com/huang/nettyTest/dto/RequestDto.java
@@ -1,5 +1,18 @@
 package com.huang.nettyTest.dto;
 
-public class RequestDto {
+import lombok.*;
 
+/**
+ * 客户端请求实体类
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Getter
+public class RequestDto {
+        //接口名称
+        private String interfaceName;
+        //方法名
+        private String method;
 }
--- src/main/java/com/huang/nettyTest/dto/ResponseDto.java
+++ src/main/java/com/huang/nettyTest/dto/ResponseDto.java
@@ -1,4 +1,16 @@
 package com.huang.nettyTest.dto;
 
+import lombok.*;
+
+/**
+ * 服务端响应实体类
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class ResponseDto {
+    //响应消息
+    private String message;
 }
